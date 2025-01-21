6f4031ec4af0a6caa83ee570b157eb26c6c2197f
Abdullah <dev.abdullllah@gmail.com>
2025-01-21 23:22:57 +0600

    remove dahbaord

--- app/(dashboard)/Providers.tsx
+++
@@ -1,8 +0,0 @@
-import { SessionProvider } from "next-auth/react";
-import React from "react";
-
-const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <SessionProvider>{children}</SessionProvider>;
-};
-
-export default Providers;
